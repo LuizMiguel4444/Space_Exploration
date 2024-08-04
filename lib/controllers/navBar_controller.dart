20d37544f9bf3bbc6fb37179b08308880c4dc9c6
luizmiguel4444 <luizmiguel4444@gmail.com>
2024-08-04 15:41:29 +0000

    feat: add nova função ao controlador

--- lib/controllers/navBar_controller.dart
+++ lib/controllers/navBar_controller.dart
@@ -4,6 +4,10 @@ import 'package:get/get.dart';
 
 class NavBarController extends GetxController {
   var currentIndex = 1.obs;
+
+  void updateIndex(int index) {
+    currentIndex.value = index;
+  }
 }
 
 final navBarController = Get.put(NavBarController());
